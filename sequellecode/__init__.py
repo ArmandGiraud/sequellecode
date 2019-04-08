d4f1073e84acdb1f9a622db9e843284459059c2c
Armand Giraud <armand.giraud.ag@gmail.com>
2019-04-08 15:36:37 +0200

    add package data manifest

--- sequellecode/__init__.py
+++ sequellecode/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for sequelle code."""
-
-from .sequellecode import SequellText
-
-__author__ = """Armand Giraud"""
-__email__ = 'armand.giraud.ag@gmail.com'
-__version__ = '0.1.0'
+""
